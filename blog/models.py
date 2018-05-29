ca238f83e8135eb0f54051e814754f67acbd0d71
Daw <varg.aba.leo@gmail.com>
2018-05-29 12:16:17 -0700

    Added views to create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,22 @@ from django.utils import timezone
 from django.shortcuts import render
 
 class Post(models.Model):
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-	title = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
-	published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-	def publish(self):
-		self.published_date = timezone.now()
-		self.save()
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
-	def __str__(self):
-		return self.title
+    def publish(self):
+            self.published_date = timezone.now()
+            self.save()
+
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
@@ -30,5 +34,4 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
+    
